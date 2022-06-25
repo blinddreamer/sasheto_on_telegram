dc2dc553568290a8018513beda3d5b9a26a69e14
Nikolay Apostolov <blinddreamer.huku.rocks>
2022-06-25 19:30:50 +0300

    sasheto_can_do_more_stuff

---
+++ sasheto_on_epic.py
@@ -0,0 +1,45 @@
+from epicstore_api import EpicGamesStoreAPI
+from datetime import datetime
+
+
+def sasheto_epik():
+    #Fetches current free games from the store
+    api = EpicGamesStoreAPI()
+    sasheto_free = ""
+
+    free_games = api.get_free_games()['data']['Catalog']['searchStore']['elements']
+    for game in free_games:
+        game_name = game['title']
+
+        for image in game['keyImages']:
+            if image['type'] == 'Thumbnail':
+                game_thumbnail = image['url']
+        game_price = game['price']['totalPrice']['fmtPrice']['originalPrice']
+        try:
+            game_promotions = game['promotions']['promotionalOffers']
+            upcoming_promotions = game['promotions']['upcomingPromotionalOffers']
+            if not game_promotions and upcoming_promotions:
+                # Promotion is not active yet, but will be active soon.
+                promotion_data = upcoming_promotions[0]['promotionalOffers'][0]
+                start_date_iso, end_date_iso = (
+                    promotion_data['startDate'][:-1], promotion_data['endDate'][:-1]
+                )
+                # Remove the last "Z" character so Python's datetime can parse it.
+                start_date = datetime.fromisoformat(start_date_iso)
+                end_date = datetime.fromisoformat(end_date_iso)
+                sasheto_free+=('{} ({}) will be free from {} to {} UTC.'.format(
+                    game_name, game_price, start_date, end_date
+                ))
+                sasheto_free+="\n"
+            else:
+                sasheto_free+=('{} ({}) is FREE now.'.format(
+                    game_name, game_price
+                ))
+                sasheto_free+="\n"
+        except TypeError:
+            pass
+            # or
+            #print('No discounts for this game')
+            # your choice
+
+    return sasheto_free
--- sasheto_on_telegram.py
+++ sasheto_on_telegram.py
@@ -2,11 +2,12 @@ import telegram.ext
 import os
 import requests
 import re
+from sasheto_on_epic import sasheto_epik
 
 TOKENO = os.environ['TOKEN']
 
 def start(update, context):
-    update.message.reply_text("....и ти ли си НЕумен като Гнома?")
+    update.message.reply_text("....НЕумен.... Гнома?")
 
 def help(update, context):
     update.message.reply_text("""
@@ -15,13 +16,16 @@ def help(update, context):
     /help -> This Message
     /fishing -> Information About.. well Fishing
     /dogo -> Dog Pics EVERYWHERE!
+    /epic -> Free Games
     """)
 
+#dogo
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()
     url = contents['url']
     return url
 
+#dogo
 def get_image_url():
     allowed_extension = ['jpg','jpeg','png']
     file_extension = ''
@@ -30,21 +34,26 @@ def get_image_url():
         file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
 
-def fishing(update, context):
-    update.message.reply_text("Всичко в риболова смърди!\nРибата смърди. Вашата стръв смърди – независимо дали е старо синьо сирене, лепкави червеи, рибни пелети или люти протеинови топчета.\nРиболовците вярват, че колкото по-отвратително мирише стръвта им толкова по-апетитна хапка за рибите е тя.\nА, ако имате и късмета да хванете риба – след това и кепчето ви ще се усмърди (особено ако го оставите да изсъхне на слънце).\nДрехите ви и те съвсем скоро ще започнат да смърдят, ако си бършете ръцете от крачолите на панталона.\nЗа капак и вие ще започнете леко да понамирисвате. Неизбежно е, колкото и да се стараете да поддържате чистота!")
-
 def dogo(update, context):
     url = get_image_url()
     chat_id = update.message.chat_id
     context.bot.send_photo(chat_id=chat_id, photo=url)
 
+def fishing(update, context):
+    update.message.reply_text("Всичко в риболова смърди!\nРибата смърди. Вашата стръв смърди – независимо дали е старо синьо сирене, лепкави червеи, рибни пелети или люти протеинови топчета.\nРиболовците вярват, че колкото по-отвратително мирише стръвта им толкова по-апетитна хапка за рибите е тя.\nА, ако имате и късмета да хванете риба – след това и кепчето ви ще се усмърди (особено ако го оставите да изсъхне на слънце).\nДрехите ви и те съвсем скоро ще започнат да смърдят, ако си бършете ръцете от крачолите на панталона.\nЗа капак и вие ще започнете леко да понамирисвате. Неизбежно е, колкото и да се стараете да поддържате чистота!")
+
+def epic(update, context):
+    update.message.reply_text(sasheto_epik())
+
 updater = telegram.ext.Updater(TOKENO, use_context=True)
 disp = updater.dispatcher
+
 
 disp.add_handler(telegram.ext.CommandHandler("start", start))
 disp.add_handler(telegram.ext.CommandHandler("help", help))
 disp.add_handler(telegram.ext.CommandHandler("fishing", fishing))
 disp.add_handler(telegram.ext.CommandHandler("dogo", dogo))
+disp.add_handler(telegram.ext.CommandHandler("epic", epic))
 
 updater.start_polling()
 updater.idle()
